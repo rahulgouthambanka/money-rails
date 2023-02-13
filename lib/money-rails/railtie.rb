78398143c02bf0e3f8521bd0f3b76dec5a5a111e
Rahul Goutham Banka <rahulgoutham.b@gmail.com>
2023-02-13 18:47:30 +0100

    feat: call _write_attribute when write_attribute called

--- lib/money-rails/railtie.rb
+++ lib/money-rails/railtie.rb
@@ -3,5 +3,12 @@ module MoneyRails
     initializer 'moneyrails.initialize', after: 'active_record.initialize_database' do
       MoneyRails::Hooks.init
     end
+
+    config.to_prepare do |_app|
+      ActiveSupport.on_load(:active_record) do
+        require_relative "active_model/attributes"
+        ::ActiveModel::Attributes.prepend(MoneyRails::ActiveModel::Attributes)
+      end
+    end
   end
 end
